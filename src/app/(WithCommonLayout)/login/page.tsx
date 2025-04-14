fd383f1dbe4257a02dd7ed33ae47a1d51f1a1e45
Md Sujon Mia <sujon258549@gmail.com>
2025-04-14 21:47:32 +0600

    registraction form

---
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/modules/shered/Signup";
+
+const Login = () => {
+  return (
+    <div>
+      <LoginForm></LoginForm>
+    </div>
+  );
+};
+
+export default Login;
